e2cde5b91bc7dffe9fb53840c1a7eb8468e463dd
root <sumarokov.vp@gmail.com>
2022-05-15 08:32:19 +0000

    init

---
+++ Schemas/ErpSalesa7f7103bSection/ErpSalesa7f7103bSection.js
@@ -0,0 +1,8 @@
+define("ErpSalesa7f7103bSection", [], function() {
+	return {
+		entitySchemaName: "ErpSales",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
