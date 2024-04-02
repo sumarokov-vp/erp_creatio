a28f51d583bdb6128768c9ad8b1f9258009c6cef
sumarokov <sumarokov.vp@gmail.com>
2024-04-02 17:42:41 +0500

    maybe last

--- install_db_triggers/Sale/executors_before.sql
+++ install_db_triggers/Sale/executors_before.sql
@@ -3,15 +3,20 @@ CREATE OR REPLACE FUNCTION "erp_executors_before" ()
     AS $$
 
 BEGIN
+  IF NEW."ErpPercent" > 0 THEN
+    SELECT "ErpTotal" * NEW."ErpPercent" / 100 INTO NEW."ErpAmount"
+    FROM "public"."ErpSales"
+    WHERE "Id" = NEW."ErpSaleId";
+  END IF;
 
-    select sale."ErpRegistered"
-    into NEW."ErpRegistered"
-    from "public"."ErpSales" sale
-    WHERE sale."Id" = NEW."ErpSaleId";
+  select sale."ErpRegistered"
+  into NEW."ErpRegistered"
+  from "public"."ErpSales" sale
+  WHERE sale."Id" = NEW."ErpSaleId";
 
 
 
-    RETURN NEW;
+  RETURN NEW;
 END;
 $$
 LANGUAGE plpgsql;
